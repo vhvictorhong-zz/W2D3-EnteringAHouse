0eacc9b9a011bfe98c27c09203e4a9cd5f410f64
victorhong1988 <victorhong1988@gmail.com>
2016-11-09 18:05:23 -0500

    Add bar button to go back to root controller

--- EnteringAHouse/ViewController.m
+++ EnteringAHouse/ViewController.m
@@ -17,8 +17,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(popBackToRootController)];
+    
 }
 
+-(void)popBackToRootController {
+    
+    [self.navigationController popToRootViewControllerAnimated:YES];
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
